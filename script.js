564ca0b3cadc87a63c13dc7fcc36c7262ca4a857
Solangie07 <solmillanbotero@gmail.com>
2026-04-13 15:18:11 +0000

    reemplazo

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 // Crear las listas de imágenes, títulos y frases
 const imagenes = [
-  "https://www.postposmo.com/wp-content/uploads/2020/09/Especies-end%C3%A9micas-de-colombia-3.jpg",
-  "https://www.postposmo.com/wp-content/uploads/2020/09/Especies-end%C3%A9micas-de-colombia-4.jpg",
+  "https://th.bing.com/th/id/OIP.wxhy0ugWhu8RIm1gYl_voQHaE7?w=242&h=180&c=7&r=0&o=7&pid=1.7&rm=3",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyCAastAYEqOnrlpcqV-A-T3BoUmeCCmnRDg&s",
+"https://static.vecteezy.com/system/resources/thumbnails/067/960/074/small/majestic-arctic-wolf-standing-in-snowy-landscape-photo.jpg",
+ "https://media.admagazine.com/photos/65d52f225cbe5d11597bf45d/4:3/w_3000,h_2250,c_limit/Colibr%C3%AD%20y%20leyenda%20maya.jpg",
+"https://humanidades.com/wp-content/uploads/2017/03/serpiente-azul-1-e1564541777289-800x400.jpg",
 ];
 
 const titulos = [
-  "Tortuga morrocoy",
-  "Ranita venenosa",
+  "LEÓN",
+  "MARIPOSA",
+  "LOBO",
+  "COLIBRÍ",
+  "SERPIENTE",
 ];
 
 const frases = [
-  "Te alimentas de plantas, frutas, hojas y flores. Vives en bosques de climas cálidos y prefieres la tierra que el agua.",
-  "Eres pequeña pero tu extravagancia no pasa desapercibida. Vives en bosques húmedos y lluviosos y tienes veneno en la piel.",
+  "El león representa corage y liderazgo.",
+  "La mariposa significa renovación y transformación.",
+  "El lobo representa lealtad y trabajo en equipo ",
+  "El colibrí representa resiliencia y alegria ",
+  "la serpiente representa tentación y renovación",
 ];
 
 // Trae los elementos del HTML que tienen ese id
@@ -27,7 +36,7 @@ function numeroRandom(min, max) {
 
 function generarPrediccion() {
   // Para elegir la misma posición en todas las listas, generar un solo número aleatorio
-  const indice = numeroRandom(0, frases.length); // 0, 1, 2, 3
+  const indice = numeroRandom(0, frases.length); // 0, 1, 2, 3, 4, 5
 
   // Reemplaza el texto del elemento 'titulo' por un título de la lista de títulos elegido aleatoriamente
   titulo.innerText = titulos[indice]; // Entre los [] se pasa el número aleatorio generado arriba
